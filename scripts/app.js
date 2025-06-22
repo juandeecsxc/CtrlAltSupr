e34483eda4b3f1f60075d3617e1f6f7750561786
MatiusDev <mateomonsalve502@gmail.com>
2025-06-22 02:18:09 -0500

    New: Start menu finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,38 @@
+import './bulma.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const audio = document.getElementById('theme-audio');
+  /* Elements */
+
+  // Intro Section
+  const intro = document.getElementById('intro');
+  const themeAudio = document.getElementById('theme-audio');
   const mute = document.getElementById('mute');
   const unmute = document.getElementById('unmute');
 
+  const continueButton = document.getElementById('continue');
+  const instructions = document.getElementById('instructions');
+  const startGame = document.getElementById('start-game');
+  const startGameAudio = document.getElementById('start-game-audio');
+  const mask = document.querySelector('.white-mask');
+
+  // Game Section
+  const game = document.getElementById('game');
+
+  /* Variables */
   let audioStarted = false;
 
+  /* Init */
+  init();
+
+  /* Functions */
+  function init() {
+    instructions.classList.add('is-active');
+  }
+
   function startAudio() {
-    audio.volume = 0.15;
     if (!audioStarted) {
-      audio.play()
+      themeAudio.volume = 0.1;
+      themeAudio.play()
         .then(() => {
           console.log('Audio de juego iniciado correctamente.');
           audioStarted = true;
@@ -18,17 +42,32 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
   }
-  startAudio();
 
-  mute.addEventListener('click', () => {
-    audio.muted = true;
+  function muteAudio() {
+    themeAudio.muted = true;
     mute.style.display = 'none';
     unmute.style.display = 'block';
-  });
+  }
 
-  unmute.addEventListener('click', () => {
-    audio.muted = false;
+  function unmuteAudio() {
+    themeAudio.muted = false;
     mute.style.display = 'block';
     unmute.style.display = 'none';
+  }
+
+  /* Event listeners */
+  mute.addEventListener('click', () => muteAudio());
+  unmute.addEventListener('click', () => unmuteAudio());
+  continueButton.addEventListener('click', () => {
+    instructions.classList.remove('is-active');
+    startAudio();
+  });
+  startGame.addEventListener('click', () => {
+    themeAudio.pause();
+    startGameAudio.volume = 0.5;
+    startGameAudio.play();
+    intro.style.display = 'none';
+    mask.style.display = 'none';
+    game.style.display = 'block';
   });
 });
---
+++ scripts/bulma.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const modals = document.querySelectorAll('.modal') || [];
+  const triggers = document.querySelectorAll('.js-modal-trigger') || [];
+  const closeTargets = document.querySelectorAll('.modal-close, .modal-card-head .delete, .modal-card-foot .button') || [];
+  
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    modals.forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  triggers.forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  closeTargets.forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if(event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+});
